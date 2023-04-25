2011d6c30dcd67a58ce57042615c6e4954058336
Chris <chrisk.developer@gmail.com>
2023-04-25 15:23:07 -0700

    Including footer and navbar

---
+++ src/app/common/components/navbar/navbar.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar.component';
+
+
+
+@NgModule({
+  declarations: [
+    NavbarComponent
+  ],
+  exports: [
+    NavbarComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class NavbarModule { }
